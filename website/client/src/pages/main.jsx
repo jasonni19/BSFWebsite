28720556b3581e9aa9126a320e5c79cbe52e48ed
jasonni <jasonni19@g.ucla.edu>
2024-12-24 02:44:11 -0800

    publishing

--- website/client/src/pages/main.jsx
+++ website/client/src/pages/main.jsx
@@ -1,32 +1,27 @@
-import React from "react"
+import React from "react";
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from './HomePage.jsx'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './HomePage.jsx';
 import AboutPage from "./AboutPage.jsx";
 import BoardPage from "./BoardPage.jsx";
-import TripsPage from "./TripsPage.jsx"
+import TripsPage from "./TripsPage.jsx";
 import SupportPage from "./SupportPage.jsx";
 
-
 import ScrollToTop from '../components/Scrolltotop.jsx'; // Import the ScrollToTop component
-import '../styles/App.css'
+import '../styles/App.css';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-      
-        <Router>
-        <ScrollToTop/>
-          <Routes>
-            <Route path="/" element={<HomePage/>} />
-            <Route path="/about" element={<AboutPage/>} />
-            <Route path="/who-we-are" element={<BoardPage/>} />
-            <Route path="/trips" element={<TripsPage/>} />
-            <Route path ="/support-us" element = {<SupportPage/>} />
-
-          </Routes>
-        </Router>
-        
-    
+    <Router>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/who-we-are" element={<BoardPage />} />
+        <Route path="/trips" element={<TripsPage />} />
+        <Route path="/support-us" element={<SupportPage />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
