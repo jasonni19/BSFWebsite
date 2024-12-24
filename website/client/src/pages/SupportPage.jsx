d3eb7f529b65c9f4fb3341cc9430efa2406f5a00
jasonni <jasonni19@g.ucla.edu>
2024-12-23 18:53:27 -0800

    tiny bug fix

--- website/client/src/pages/SupportPage.jsx
+++ website/client/src/pages/SupportPage.jsx
@@ -18,7 +18,7 @@ const SupportPage = () => {
                     Thank you so much for your interest in supporting Bruin Sportfishing!
                 </h1>
 
-                <p className="element-to-animate opacity-0 text-xl lg:text-3xl mt-8 text-center w-[50%] mb-4">
+                <p className="element-to-animate opacity-0 text-base lg:text-3xl mt-8 text-center w-[50%] mb-4">
                     As a student-run organization, funding is always a challenge for Bruin Sportfishing. From transportation and gear rentals for our fishing trips to providing bait, tackle, and educational materials for our meetings, most of our club’s expenses come directly from our members' pockets. Your support will allow us to make fishing trips, like deep-sea excursions and local outings, more accessible to all Bruins and help us expand fishing education without financial barriers!
                 </p>
 
